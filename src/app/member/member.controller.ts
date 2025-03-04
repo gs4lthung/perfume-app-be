a2c4a2b05fa04b83e5c628f3f51ca38216734d37
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-03-05 01:43:30 +0700

    feat: add password update endpoint to member controller

--- src/app/member/member.controller.ts
+++ src/app/member/member.controller.ts
@@ -7,10 +7,11 @@ import {
   Param,
   Delete,
   HttpCode,
+  Put,
 } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { CreateMemberDto } from './dto/create-member.dto';
-import { UpdateMemberDto } from './dto/update-member.dto';
+import { UpdateMemberDto, UpdatePasswordDto } from './dto/update-member.dto';
 
 @Controller('member')
 export class MemberController {
@@ -35,6 +36,14 @@ export class MemberController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateMemberDto: UpdateMemberDto) {
     return this.memberService.update(id, updateMemberDto);
+  }
+
+  @Put(':id')
+  updatePassword(
+    @Param('id') id: string,
+    @Body() updateMemberDto: UpdatePasswordDto,
+  ) {
+    return this.memberService.updatePassword(id, updateMemberDto);
   }
 
   @Delete(':id')
